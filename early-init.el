af8ba55ba815a7b631bad70f429f0a33d9563844
Javier Castilla <jcastp@pm.me>
2024-11-29 08:09:02 +0100

    custom.el load changed, attributions added

--- early-init.el
+++ early-init.el
@@ -10,9 +10,12 @@
 (setq gc-cons-percentage 0.5)
 
 ;; the custom.el file location
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(when (file-exists-p custom-file)
-  (load custom-file))
+;;(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+;; (when (file-exists-p custom-file)
+;;   (load custom-file))
+;; from Prot's article - https://protesilaos.com/codelog/2024-11-28-basic-emacs-configuration/
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file :no-error-if-file-is-missing)
 
 ;; Set the frame size according to pixels and not based on the size of the font.
 (setq frame-resize-pixelwise t)
