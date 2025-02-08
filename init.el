23dbb54de201170121a7af22b5df9a4731df2e5d
Javier Castilla <jcastp@pm.me>
2025-02-08 10:43:29 +0100

    use-package config working

--- init.el
+++ init.el
@@ -13,24 +13,15 @@
 (setq inhibit-splash-screen t
       inhibit-startup-screen t)
 
-;; straight installation
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name
-        "straight/repos/straight.el/bootstrap.el"
-        (or (bound-and-true-p straight-base-dir)
-            user-emacs-directory)))
-      (bootstrap-version 7))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
 
-(straight-use-package 'use-package)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; package initialization
+(require 'package)
+(package-initialize)
+
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
 
 ;; remove the packages warning messages
 (add-to-list 'display-buffer-alist
